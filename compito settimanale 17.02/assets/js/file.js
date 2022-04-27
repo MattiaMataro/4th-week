f8fe5b43b0d0d405fbd8e1ff02eba97a7e83f950
MattiaMataro <98821819+MattiaMataro@users.noreply.github.com>
2022-04-27 13:06:55 +0200

    Add files via upload

---
+++ compito settimanale 17.02/assets/js/file.js
@@ -0,0 +1,34 @@
+
+function audio1() {
+    var myaudio = document.getElementById("audio");
+    myaudio.setAttribute("src", "assets/media/heartbeat.mp3");
+    myaudio.play();
+}
+function audio2() {
+    var myaudio = document.getElementById("audio");
+    myaudio.setAttribute("src", "assets/media/honky_tonky_piano.mp3");
+    myaudio.play();
+}
+function audio3() {
+    var myaudio = document.getElementById("audio");
+    myaudio.setAttribute("src", "assets/media/mastermind.mp3");
+    myaudio.play();
+}
+
+function video1() {
+    var myvideo = document.getElementById("video");
+    myvideo.setAttribute("src", "assets/media/by_night.mp4");
+    myvideo.play();
+}
+
+function video2() {
+    var myvideo = document.getElementById("video");
+    myvideo.setAttribute("src", "assets/media/Flight.mp4");
+    myvideo.play();
+}
+function video3() {
+    var myvideo = document.getElementById("video");
+    myvideo.setAttribute("src", "assets/media/Sea.mp4");
+    myvideo.play();
+}
+
